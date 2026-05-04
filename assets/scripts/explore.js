f0a0d644773ef83b8423b4aea5ab7737833344e1
Nhan Tri Danh <57444770+dnhan1707@users.noreply.github.com>
2026-05-03 22:49:32 -0700

    feat: shows list of voices

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,21 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  const synth = window.speechSynthesis;
+  const voiceSelect = document.querySelector("select")
+  let voices = [];
+
+  function loadVoices() {
+    voices = synth.getVoices();
+    voices.forEach(voice => {
+      const option = document.createElement("option");
+      option.textContent = `${voice.name} (${voice.lang})`;
+      if (voice.default) option.textContent += " — DEFAULT";
+      option.setAttribute("data-lang", voice.lang);
+      option.setAttribute("data-name", voice.name);
+      voiceSelect.appendChild(option);
+    });
+  }
+
+  synth.onvoiceschanged = loadVoices;
 }
