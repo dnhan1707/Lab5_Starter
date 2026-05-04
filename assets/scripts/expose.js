82be2d2e08e70ec5f7c3f26f0129848313d4b9dd
Nhan Tri Danh <57444770+dnhan1707@users.noreply.github.com>
2026-05-03 22:03:45 -0700

    feat: Sound icon changes based on input bar value

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -5,6 +5,7 @@ window.addEventListener('DOMContentLoaded', init);
 function init() {
   // TODO
   const baseImagesLink = "assets/images/";
+  const baseIconLink = "assets/icons/"
 
   // Make image changing based one selected Horn
   const hornSelectTag = document.getElementById("horn-select");
@@ -34,6 +35,23 @@ function init() {
         imageTag.src = baseImagesLink + "no-image.png";
         imageTag.alt = "no image pic";
         break;
+    }
+  })
+
+  // Sounds change
+  const volumeInput = document.getElementById("volume");
+  const volumeImage = document.querySelector("#volume-controls img");
+
+  volumeInput.addEventListener("input", function () {
+    const volumeValue = this.value;
+    if (volumeValue == 0) {
+      volumeImage.src = baseIconLink + "volume-level-0.svg";
+    } else if (volumeValue < 33) {
+      volumeImage.src = baseIconLink + "volume-level-1.svg";
+    } else if (volumeValue < 67) {
+      volumeImage.src = baseIconLink + "volume-level-2.svg";
+    } else {
+      volumeImage.src = baseIconLink + "volume-level-3.svg";
     }
   })
 }
