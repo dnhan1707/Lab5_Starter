d79c2a7a5fdf42465af307b68778ec4a1f2ece0d
Nhan Tri Danh <57444770+dnhan1707@users.noreply.github.com>
2026-05-03 21:51:15 -0700

    feat: Image changes based on image selection

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,36 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  const baseImagesLink = "assets/images/";
+
+  // Make image changing based one selected Horn
+  const hornSelectTag = document.getElementById("horn-select");
+  const imageTag = document.querySelector("img");
+
+  hornSelectTag.addEventListener("change", function () {
+    const selectedValue = this.value;
+    
+    // set image
+    switch(selectedValue) {
+      case "air-horn":
+        imageTag.src = baseImagesLink + "air-horn.svg";
+        imageTag.alt = "air horn pic";
+        break;
+
+      case "car-horn":
+        imageTag.src = baseImagesLink + "car-horn.svg";
+        imageTag.alt = "car horn pic";
+        break;
+      
+      case "party-horn":
+        imageTag.src = baseImagesLink + "party-horn.svg";
+        imageTag.alt = "party horn pic";
+        break;
+      
+      default:
+        imageTag.src = baseImagesLink + "no-image.png";
+        imageTag.alt = "no image pic";
+        break;
+    }
+  })
 }
